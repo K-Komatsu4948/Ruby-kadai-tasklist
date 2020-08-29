b8825ca051f042921c6650b26f3a1e8558731996
natsume4948 <kazu.100817@gmail.com>
2020-08-29 13:43:39 +0000

    version

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -49,7 +49,7 @@ class TasksController < ApplicationController
   private
   
   def task_params
-    params.require(:task).permit(:commit)
+    params.require(:task).permit(:content)
   end
   
 end

26f2083f9d397a6aba7be6da06bb082627a88145
natsume4948 <kazu.100817@gmail.com>
2020-08-30 08:07:30 +0000

    add

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,7 +6,6 @@ class TasksController < ApplicationController
   end
   
   def show
-  	set_task
   end
 
   def new
@@ -25,11 +24,9 @@ class TasksController < ApplicationController
   end
 
   def edit
-  	set_task
   end
   
   def update
-  	set_task
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
@@ -40,7 +37,6 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    set_task
     @task.destroy
     
     flash[:success] = 'Task は正常に削除されました'
